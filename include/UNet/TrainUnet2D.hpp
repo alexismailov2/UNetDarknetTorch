5e60f16146136143ae15d003c24d0d570f9f14d2
Alexander Ismailov <alexismailov2@gmail.com>
2020-11-21 22:47:48 +0200

    Extend interface of library by some useful functions.
    Signed-off-by: Alexander Ismailov <alexismailov2@gmail.com>

--- include/UNet/TrainUnet2D.hpp
+++ include/UNet/TrainUnet2D.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <opencv2/opencv.hpp>
+
 #include <map>
 #include <string>
 #include <vector>
+
+void CountingLabeledObjects(std::map<std::string, uint32_t>& map, std::string const& polygonInfo, bool forImage = false);
+auto ConvertPolygonsToMask(std::string const& polygonInfo, std::map<std::string, cv::Scalar> colorToClass) -> cv::Mat;
 
 void runOpts(std::map<std::string, std::vector<std::string>> params);
 auto ParseOptions(int argc, char *argv[]) -> std::map<std::string, std::vector<std::string>>;
